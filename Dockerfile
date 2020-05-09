2663d976a70008ac0126f8cf5ccfd03ae9897e20
Matt Ammons <ammons@gmail.com>
2020-05-09 12:48:47 -0400

    added spotify playlist

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ ADD config.py /
 ADD recursiveJson.py /
 ADD client_secret_862974823303-5k6td646glkbegi3j4bq83ns87q2a17g.apps.googleusercontent.com.json /
 ADD CREDENTIALS_PICKLE_FILE /
+ADD .cache-1234656043 /
 ADD spotifytube.py /
 CMD [ "python", "./spotifytube.py" ]
--- spotifytube.py
+++ spotifytube.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import pickle
 import google_auth_oauthlib.flow
@@ -7,6 +8,7 @@ import googleapiclient.errors
 import config
 import discord
 import spotipy
+import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from recursiveJson import extract_values
 
@@ -20,9 +22,13 @@ api_service_name = "youtube"
 api_version = "v3"
 client_secrets_file = "client_secret_862974823303-5k6td646glkbegi3j4bq83ns87q2a17g.apps.googleusercontent.com.json"
 
-spotify_ccm = SpotifyClientCredentials(
-    config.spotify_client_id, config.spotify_secret)
-sp = spotipy.Spotify(client_credentials_manager=spotify_ccm)
+token = util.prompt_for_user_token(config.sp_username, config.sp_scope, config.spotify_client_id, config.spotify_secret, config.sp_redirect_uri)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+else:
+    print(f'cant get token for username {config.sp_username}')
+    sys.exit()
 client = discord.Client()
 
 
@@ -43,16 +49,11 @@ def get_authenticated_service():
 youtube = get_authenticated_service()
 
 
-def get_youtube_video_data(spotify_link):
-    track_data = sp.track(spotify_link)
-
-    sp_artistname = track_data['artists'][0]['name']
-    sp_trackname = track_data['name']
-
+def get_youtube_video_data(artist_name, track_name):
     searchRequest = youtube.search().list(
         part="snippet",
         maxResults=1,
-        q=f"{sp_artistname} {sp_trackname}",
+        q=f"{artist_name} {track_name}",
     )
 
     return searchRequest.execute()
@@ -75,7 +76,7 @@ def get_existing_videos_in_playlist(playlist_id):
     return playlistQuery.execute()
 
 
-def add_video_to_playlist(yt_video_id):
+def add_video_to_youtube_playlist(yt_video_id):
     youtube.playlistItems().insert(
         part="snippet",
         body={
@@ -90,7 +91,6 @@ def add_video_to_playlist(yt_video_id):
         }
     ).execute()
 
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -103,10 +103,26 @@ async def on_message(message):
 
     trigger_string = 'https://open.spotify.com'
     if trigger_string in message.content:
-        # sp.user_playlist_add_tracks("1234656043", "08YWXePJ0DzP9Ls1ccZHT7", message.content)
-        await message.channel.send('Looking that shit up on YouTube...')
+
+        track_data = sp.track(message.content)
+        track_uri = track_data['uri']
+        sp_artistname = track_data['artists'][0]['name']
+        sp_trackname = track_data['name']
+
         try:
-            yt_data = get_youtube_video_data(message.content)
+            current_sp_tracks = sp.user_playlist_tracks(config.sp_username, config.sp_playlist_id)
+            current_sp_track_uris = extract_values(current_sp_tracks, 'uri')
+            if track_uri not in current_sp_track_uris:
+                sp.user_playlist_add_tracks(config.sp_username, config.sp_playlist_id, [track_uri])
+                await message.channel.send("Added track to Spotify playlist")
+            else:
+                await message.channel.send("Track already exists in Spotify playlist")
+        except Exception as e:
+            print(f'couldnt add track to spotify {e}')
+
+        await message.channel.send('Looking it up on YouTube...')
+        try:
+            yt_data = get_youtube_video_data(sp_artistname, sp_trackname)
             if not yt_data:
                 await message.channel.send("Sorry. I couldn't find anything")
             else:
@@ -118,12 +134,12 @@ async def on_message(message):
                 existingVideoIds = extract_values(existingVideos, "videoId")
                 if(yt_id not in existingVideoIds):
                     try:
-                        add_video_to_playlist(yt_id)
-                        await message.channel.send(f"I went ahead and added that song to the playlist for you: https://www.youtube.com/playlist?list={config.youtube_playlist_id}")
+                        add_video_to_youtube_playlist(yt_id)
+                        await message.channel.send(f"Added video to YouTube playlist")
                     except Exception as e:
                         print(f"exception when inserting into playlist: {e}")
                 else:
-                    await message.channel.send("This is old, man. Keep up.")
+                    await message.channel.send("Video already exists in YouTube playlist")
         except Exception as e:
             await message.channel.send("something went wrong. i don't care")
             print(f"{e}")
